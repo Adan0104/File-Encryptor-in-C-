cdc8fa48b82b4fd83af48499e2048a1520755d73
Adan0104 <abidinadanul@gmail.com>
2024-07-13 01:02:23 +0530

    Initial commit

---
+++ src/app/fileHandling/IO.hpp
@@ -0,0 +1,21 @@
+//used for description of header files
+
+#ifndef IO_HPP
+#define IO_HPP
+
+#include<fstream>
+#include<string>
+#include<iostream>
+
+class IO{
+    public:
+        IO(const std::string &file_path);
+        ~IO();
+        std::fstream getFileStream();
+
+    private:
+        std::fstream file_stream;
+};
+
+
+#endif
